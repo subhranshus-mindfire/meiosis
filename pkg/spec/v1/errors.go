23a31f93c6f02e49578daaab648797b5f9efc41c
subhranshu <subhranshus@mindfiresolutions.com>
2026-09-03 23:08:45 +0530

    feat: add error for invalid Ed25519 signature

--- pkg/spec/v1/errors.go
+++ pkg/spec/v1/errors.go
@@ -13,4 +13,5 @@ var (
 	ErrInvalidVerdict     = errors.New("invalid verdict")
 	ErrInvalidKey         = errors.New("invalid Ed25519 key")
 	ErrKeyMismatch        = errors.New("Ed25519 public and private keys do not match")
+	ErrInvalidSignature   = errors.New("invalid Ed25519 signature")
 )

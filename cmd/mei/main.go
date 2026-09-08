b2114dc5a12a75353a03c3c9e4023fe5a1f7258a
subhranshu <subhranshus@mindfiresolutions.com>
2026-09-08 18:15:32 +0530

    feat: implement mei CLI with subcommands and Viper configuration binding

--- cmd/mei/main.go
+++ cmd/mei/main.go
@@ -1,4 +1,15 @@
 package main
 
-// The CLI entrypoint is intentionally minimal until the command surface is implemented.
-func main() {}
+import (
+	"fmt"
+	"os"
+
+	"github.com/mindfire-test/meiosis/internal/cli"
+)
+
+func main() {
+	if err := cli.New(nil).Execute(); err != nil {
+		_, _ = fmt.Fprintln(os.Stderr, err)
+		os.Exit(1)
+	}
+}
